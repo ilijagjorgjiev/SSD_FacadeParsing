a33b4bda18590d918f092ae32a02b4b52a9e9168
Gjorgjiev Ilija <ilija@tapeda.datascience.ch.novalocal>
2019-12-18 00:37:17 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Signle Shot MultiBox Detector-SSD Fascade Parsing',
+      version='1.0',
+      description='Signle Shot MultiBox Detector Project',
+      author='Gjorgjiev Ilija',
+      author_email='ilija.gjorgjiev@epfl.ch',
+      url='github.com/ilijagjorgjiev',
+      packages=['ssd_project'],
+      requires=[
+          'tqdm',
+          'torch',
+          'cv2',
+          'PIL',
+          'numpy',
+          'argparse',
+          'matplotlib',
+          'torchvision',
+          'imageio',
+          'os',
+          'scipy',
+          'pandas',
+          'itertools',
+          'glob',
+          'math,
+          
+      ])
